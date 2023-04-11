bd71160c98e9b9dc6ad83310a4bc580a23bcc395
HomeLanderz <86560837+HomeLanderz@users.noreply.github.com>
2023-04-11 12:33:55 +0300

    Fix: deployment issue

--- src/components/shared/AppHeader.jsx
+++ src/components/shared/AppHeader.jsx
@@ -9,7 +9,6 @@ import Button from "../reusable/Button";
 
 const AppHeader = () => {
   const [showMenu, setShowMenu] = useState(false);
-  const [showModal, setShowModal] = useState(false);
   const [activeTheme, setTheme] = useThemeSwitcher();
 
   function toggleMenu() {
@@ -19,7 +18,7 @@ const AppHeader = () => {
       setShowMenu(false);
     }
   }
-	
+
   return (
     <motion.nav
       initial={{ opacity: 0 }}
